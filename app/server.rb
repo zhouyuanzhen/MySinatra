93dae657fe5ec049822b1ad92993c1044a4741cf
Yuanzhen ZHou <szrednick+zhouyuanzhen@gmail.com>
2021-07-28 17:30:26 +0800

    Add json

--- app/server.rb
+++ app/server.rb
@@ -1,7 +1,15 @@
 # Requires the Gemfile
 require 'bundler' ; Bundler.require
+require 'JSON'
 
 # By default Sinatra will return the string as the response.
 get '/hello-world' do
     "Hello World!"
+end
+
+get '/hello-world-json' do
+    content_type :json # Content-Type: application/json;charset=utf-8
+
+    # Use to_json to generate JSON based on the Ruby hash
+    {greeting: 'Hello World! 2021!'}.to_json
 end
